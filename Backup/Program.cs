c64f018363cd049c5a50bada0e9d87f5ee6c2e5e
gianjey <gianjey@gmail.com>
2018-01-15 14:46:03 +0100

    Initial Commit

---
+++ Backup/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace Downloader
+{
+
+    public struct Link
+    {
+        public string link, valore;
+
+        public Link(string Link, string Valore)
+        {
+            link = Link;
+            valore = Valore;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //La lista di url da scaricare
+            List<Link> linkToDownload = new List<Link>();
+
+            //Leggo il file di testo
+            try
+            {
+
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(@"C:\Progetti\MyProgetti\Downloader\Downloader\bin\Link\Link.txt"))
+                {
+                    String line;
+                    // Read and display lines from the file until the end of 
+                    // the file is reached.
+                    int i = 0;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        i++;
+                        Console.WriteLine(line);
+                        linkToDownload.Add(new Link(line, i + ".jpeg"));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+           
+            //linkToDownload.Add(new Link("http://www.carlsalter.com/download.asp?fileid=065100108121032049053048032085084073076073084065073082069032112097114116115046112100102", "Adly 150 UTILITAIRE parts"));
+
+            //Ciclo e lancio il tread
+            foreach (Link item in linkToDownload)
+            {
+                Thread paramThreadDownlaoder = new Thread(threadDownloaderParametrico);
+                paramThreadDownlaoder.Start(item);
+            }
+
+            Console.WriteLine("Premi un tasto");
+            Console.ReadLine();
+
+        }
+
+        private static void threadDownloaderParametrico(object o)
+        {
+            Link link = (Link)o;
+            Console.WriteLine("threadDownloaderParametrico: {0}", link.valore);
+            Utils.DownloadFile(link.link, link.valore.Trim());
+        }
+    }
+}
---
+++ Backup/Utils.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using Downloader.Properties;
+
+namespace Downloader
+{
+    public class Utils
+    {
+        public static string ConcatUrls(string firstPart, string secondPart)
+        {
+            if (firstPart.EndsWith("/") == false)
+            {
+                if (secondPart.StartsWith("/") == false)
+                {
+                    return firstPart + "/" + secondPart;
+                }
+                return firstPart + secondPart;
+            }
+            if (secondPart.StartsWith("/") == true)
+            {
+                firstPart = firstPart.TrimEnd(new char[] { '/' });
+            }
+            return firstPart + secondPart;
+        }
+
+        public static bool? boolValue(string outVal)
+        {
+            if (String.IsNullOrEmpty(outVal))
+                return null;
+            else
+            {
+                string valore = outVal.Trim().ToUpper();
+                if (valore != "NO" && valore != "SI")
+                    throw new SystemException("Boolean non valido");
+                else
+                    return valore == "SI";
+            }
+        }
+
+        public static int? ToNullableInt32(string s)
+        {
+            int i;
+            if (Int32.TryParse(s, out i)) return i;
+            return null;
+        }
+
+        public static decimal? ToNullableDecimal(string s)
+        {
+            decimal retOut;
+            if (decimal.TryParse(s, out retOut)) return retOut;
+            return null;
+
+        }
+
+        public static decimal ToDecimal(string s)
+        {
+            decimal retOut;
+            decimal.TryParse(s, out retOut);
+            return retOut;
+
+
+        }
+
+        public static bool DownloadImage(string urlImage, string ImageName)
+        {
+            //Scarico l'immagine 
+            string ImagesDir = Path.GetFullPath(Settings.Default.ImagesDirectory);
+            //Controllo l'esistenza della directory specificata
+            if (!Directory.Exists(ImagesDir))
+            {
+                DirectoryInfo currPath = Directory.GetParent(ImagesDir);
+                if (currPath.FullName.Equals(currPath.Parent.FullName, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    currPath = currPath.Parent;
+                }
+                if (Directory.Exists(currPath.FullName))
+                {
+                    Directory.CreateDirectory(ImagesDir);
+                }
+                else
+                {
+                    throw new DirectoryNotFoundException("Path specificato non valido.");
+                }
+            }
+
+            string ImageSavePath = Path.Combine(ImagesDir, ImageName);
+            if (File.Exists(ImageSavePath))
+                return true;
+
+            try
+            {
+                Console.Write("Download immagine: " + urlImage);
+                WebRequest requestPic = WebRequest.Create(urlImage);
+
+                using (WebResponse resp = requestPic.GetResponse())
+                {
+                    using (Stream ResponseStream = resp.GetResponseStream())
+                    {
+                        FileStream writeStream = new FileStream(ImageSavePath, FileMode.Create, FileAccess.Write);
+                        Byte[] readBuffer = new Byte[256];
+                        // Read from buffer
+                        int count = ResponseStream.Read(readBuffer, 0, 256);
+                        while (count > 0)
+                        {
+                            // get string
+                            writeStream.Write(readBuffer, 0, count);
+                            count = ResponseStream.Read(readBuffer, 0, 256);
+                        }
+                        // Release the response object resources.
+                        ResponseStream.Close();
+                        writeStream.Close();
+                    }
+                }
+
+                Console.Write(" - Scaricata");
+                Console.WriteLine();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write(" - Errore download immagine {0}, errore:{1}", urlImage, ex.Message);
+                Console.WriteLine();
+                return false;
+            }
+        }
+
+        public static bool DownloadFile(string urlFile, string FileName)
+        {
+            //Scarico l'immagine 
+            string FilesDir = Path.GetFullPath(Settings.Default.FilesDirectory);
+            //Controllo l'esistenza della directory specificata
+            if (!Directory.Exists(FilesDir))
+            {
+                DirectoryInfo currPath = Directory.GetParent(FilesDir);
+                if (currPath.FullName.Equals(currPath.Parent.FullName, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    currPath = currPath.Parent;
+                }
+                if (Directory.Exists(currPath.FullName))
+                {
+                    Directory.CreateDirectory(FilesDir);
+                }
+                else
+                {
+                    throw new DirectoryNotFoundException("Path specificato non valido.");
+                }
+            }
+
+            string FilesSavePath = Path.Combine(FilesDir, FileName);
+            // Se non voglio l'aggiornamento scommenta queste righe
+            //if (File.Exists(FilesSavePath))
+            //    return true;
+
+            try
+            {
+                Console.WriteLine("Download file: " + urlFile);
+                WebRequest requestFile = WebRequest.Create(urlFile);
+
+                using (WebResponse resp = requestFile.GetResponse())
+                {
+                    using (Stream ResponseStream = resp.GetResponseStream())
+                    {
+                        FileStream writeStream = new FileStream(FilesSavePath, FileMode.Create, FileAccess.Write);
+                        Byte[] readBuffer = new Byte[256];
+                        // Read from buffer
+                        int count = ResponseStream.Read(readBuffer, 0, 256);
+                        while (count > 0)
+                        {
+                            // get string
+                            writeStream.Write(readBuffer, 0, count);
+                            count = ResponseStream.Read(readBuffer, 0, 256);
+                        }
+                        // Release the response object resources.
+                        ResponseStream.Close();
+                        writeStream.Close();
+                    }
+                }
+
+                Console.WriteLine(urlFile + " - Scaricato come - " + FileName );
+                Console.WriteLine();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write(" - Errore download file {0}, errore:{1}", urlFile, ex.Message);
+                Console.WriteLine();
+                return false;
+            }
+        }
+    }
+}
+
+
+
+    
